a2344cba06d25f277d0261b42cf88897c95a0556
Wyatt Bodle <wyattbodle@Wyatts-MacBook-Pro.local>
2025-03-05 12:23:32 -0800

    added directory

--- OTHER_SRC/Labs/Lab 2/server.cpp
+++ OTHER_SRC/Labs/Lab 2/server.cpp
@@ -1,10 +1,40 @@
 #include "Adafruit_seesaw.h"
 #include "M5Core2.h"
-#include <random>
 #include <BLEDevice.h>
+#include <BLEUtils.h>
 #include <BLEServer.h>
-#include <BLE2902.h>
 
+///////////////////////////////////////////////////////////////
+// UUID's
+///////////////////////////////////////////////////////////////
+#define SERVICE_UUID "12345678-1234-5678-1234-56789abcdef0"
+#define CHARACTERISTIC_UUID "87654321-4321-6789-4321-abcdef987654"
+
+
+///////////////////////////////////////////////////////////////
+// Point Struct
+///////////////////////////////////////////////////////////////
+struct DOT {
+    int x;
+    int y;
+    int multiplier;
+    int lastX;
+    int lastY;
+};
+
+
+///////////////////////////////////////////////////////////////
+// Variables
+///////////////////////////////////////////////////////////////
+
+//Regular
+int dotSize = 10;
+boolean deviceConnected = false;
+DOT clientDot = {310, 230};
+DOT localDot = {0, 0};
+boolean screenClear = false;
+
+//Seesaw Controller
 Adafruit_seesaw ss;
 
 #define BUTTON_X 6
@@ -16,40 +46,20 @@ Adafruit_seesaw ss;
 
 unsigned long lastSelectPress = 0;  
 
-int pinkDotX = 0;
-int pinkDotY = 0;
-int blueDotX = 200;
-int blueDotY = 200;
+uint32_t button_mask = (1UL << BUTTON_START) | (1UL << BUTTON_SELECT);
 
+
+//Game states
 boolean isGameOver = false;
 double startTime = millis();
 double endTime;
 
-int pinkDotMultiplier = 1;
-int blueDotMultiplier = 1;
-String millisToTime(unsigned long millisVal);
-Point getRandomNum();
-
-uint32_t button_mask = (1UL << BUTTON_START) | (1UL << BUTTON_SELECT);
-
-// Variables for BLE
-BLEServer *bleServer;
-BLEService *bleService;
-BLECharacteristic *bleCharacteristic;
-bool deviceConnected = false;
-bool previouslyConnected = false;
-int timer = 0;
-
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-static String BLE_BROADCAST_NAME = "Wyatts M5Core2024";
-
+///////////////////////////////////////////////////////////////
 // BLE Server Callback Methods
-class MyServerCallbacks : public BLEServerCallbacks {
+///////////////////////////////////////////////////////////////
+class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer *pServer) {
         deviceConnected = true;
-        previouslyConnected = true;
         Serial.println("Device connected...");
     }
     void onDisconnect(BLEServer *pServer) {
@@ -58,32 +68,79 @@ class MyServerCallbacks : public BLEServerCallbacks {
     }
 };
 
-// BLE Characteristic Callback Methods
+///////////////////////////////////////////////////////////////
+// Forward Declarations
+///////////////////////////////////////////////////////////////
+void drawScreenTextWithBackground(String text, int backgroundColor);
+String millisToTime(unsigned long millisVal);
+Point getRandomNum();
+
+///////////////////////////////////////////////////////////////
+// Characteristic Callbacks
+///////////////////////////////////////////////////////////////
 class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
+    void onRead(BLECharacteristic *pCharacteristic) override {
+        char buffer[20];
+        snprintf(buffer, sizeof(buffer), "%d,%d", localDot.x, localDot.y);
+        pCharacteristic->setValue(buffer);
+        // Serial.printf("Client read response: %s\n", buffer);
+    }
+    void onWrite(BLECharacteristic *pCharacteristic) override {
         std::string value = pCharacteristic->getValue();
-        Serial.print("Received data from client: ");
-        Serial.println(value.c_str());
+        if (!value.empty()) {
+            sscanf(value.c_str(), "%d,%d", &clientDot.x, &clientDot.y);
+            // Serial.printf("Client wrote: x=%d, y=%d\n", clientDot.x, clientDot.y);
+        }
+    }
+    void onConnect(BLEClient *pclient)
+    {
+        M5.lcd.fillScreen(BLACK);
+        Serial.println("Device connected...");
+        deviceConnected = true;
+    }
+
+    void onDisconnect(BLEClient *pclient)
+    {
+        deviceConnected = false;
+        Serial.println("Device disconnected...");
+        drawScreenTextWithBackground("LOST connection to device.\n\nAttempting re-connection...", TFT_RED);
     }
 };
 
-// Forward Declarations
-void broadcastBleServer();
-void drawScreenTextWithBackground(String text, int backgroundColor);
+
+///////////////////////////////////////////////////////////////
+// Broadcasting BLE server to client
+///////////////////////////////////////////////////////////////
+void broadcastServer(){
+    BLEDevice::init("M5Core2_BLE_Server");
+    BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
+    );
+    pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+    pService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    BLEDevice::startAdvertising();
+    Serial.println("BLE Server is running...");
+}
 
 void setup() {
     M5.begin();
-
     Serial.begin(115200);
-    while (!Serial) {
-        delay(10);
-    }
-    Serial.println("Gamepad QT example!");
-
+    M5.Lcd.setTextSize(3);
+    ss.begin();
+    drawScreenTextWithBackground("Looking for BLE Client!" , TFT_ORANGE);
+    //Seesaw Setup
     if (!ss.begin(0x50)) {
         Serial.println("ERROR! seesaw not found");
         while (1) delay(1);
     }
+
     Serial.println("seesaw started");
 
     uint32_t version = ((ss.getVersion() >> 16) & 0xFFFF);
@@ -96,140 +153,117 @@ void setup() {
 
     ss.pinModeBulk(button_mask, INPUT_PULLUP);
     ss.setGPIOInterrupts(button_mask, 1);
+    
+    broadcastServer();
 
-    randomSeed(millis());
-
-    // Initialize M5Core2 as a BLE server
-    Serial.print("Starting BLE...");
-    BLEDevice::init(BLE_BROADCAST_NAME.c_str());
-    M5.Lcd.setTextSize(3);
-
-    // Broadcast the BLE server
-    drawScreenTextWithBackground("Initializing BLE...", TFT_CYAN);
-    broadcastBleServer();
-    drawScreenTextWithBackground("Broadcasting as BLE server named:\n\n" + BLE_BROADCAST_NAME, TFT_BLUE);
 }
 
-int last_x = 0, last_y = 0;
-
 void loop() {
-
     if(deviceConnected){
-        M5.Lcd.fillScreen(BLACK);
+        if(!screenClear){
+            M5.lcd.fillScreen(BLACK);
+            screenClear = true;
+        }
+        //Draw black dot over old area
+        M5.Lcd.fillRect(clientDot.lastX, clientDot.lastY, dotSize, dotSize, BLACK);
+        M5.Lcd.fillRect(localDot.lastX, localDot.lastY, dotSize, dotSize, BLACK);
 
-        if (!isGameOver)
-        {
-         delay(10); // Slow serial output
-    
-        // Reverse x/y values to match joystick orientation
-        int x = 1023 - ss.analogRead(14);
-        int y = 1023 - ss.analogRead(15);
-    
-        if (x > 560 && blueDotX < M5.Lcd.width() - 3)
-        {
-            blueDotX = blueDotX + (1 * blueDotMultiplier);
-        }
-        if (x < 500 && blueDotX > 3)
-        {
-            blueDotX = blueDotX - (1 * blueDotMultiplier);
-        }
-    
-        if (y < 490 && blueDotY < M5.Lcd.height() - 3)
-        {
-            blueDotY = blueDotY + (1 * blueDotMultiplier);
-        }
-        if (y > 550 && blueDotY > 3)
-        {
-            blueDotY = blueDotY - (1 * blueDotMultiplier);
-        }
-    
-        last_x = x;
-        last_y = y;
-    
-        uint32_t buttons = ss.digitalReadBulk(button_mask);
-    
-        unsigned long currentTime = millis();
-    
-        if (!(buttons & (1UL << BUTTON_SELECT))) {  // Check if SELECT button is pressed
-            if (currentTime - lastSelectPress >= 500) {  // Enforce timeout
-                Serial.println("Button SELECT pressed");
-    
-                Point rand = getRandomNum();
-                blueDotX = rand.x;
-                blueDotY = rand.y;
-    
-                lastSelectPress = currentTime;  // Update last press time
+
+        // Draw the new positions
+        M5.Lcd.fillRect(localDot.x, localDot.y, dotSize, dotSize, BLUE);
+        M5.Lcd.fillRect(clientDot.x, clientDot.y, dotSize, dotSize, PINK);
+
+        // Store last position
+        if (localDot.lastX != localDot.x) localDot.lastX = localDot.x;
+        if (localDot.lastY != localDot.y) localDot.lastY = localDot.y;
+        if (clientDot.lastX != clientDot.x) clientDot.lastX = clientDot.x;
+        if (clientDot.lastY != clientDot.y) clientDot.lastY = clientDot.y;
+
+
+        if (!isGameOver) {
+            delay(10);
+
+            int x = 1023 - ss.analogRead(14);
+            int y = 1023 - ss.analogRead(15);
+
+            if (x > 560 && localDot.x < M5.Lcd.width() - 3) {
+                localDot.x += 1 * localDot.multiplier;
             }
-        }
-    
-        if (!(buttons & (1UL << BUTTON_START))) {
-            Serial.println("Button START pressed");
-            if (blueDotMultiplier < 5)
-            {
-                blueDotMultiplier = blueDotMultiplier + 1;
-            }else
-            {
-                blueDotMultiplier = 1;
+            if (x < 500 && localDot.x > 3) {
+                localDot.x -= 1 * localDot.multiplier;
             }
-        }
-            if (sqrt(pow(pinkDotX - blueDotX, 2) + pow(pinkDotY - blueDotY, 2)) <= 20)
-            {
+            if (y < 490 && localDot.y < M5.Lcd.height() - 3) {
+                localDot.y += 1 * localDot.multiplier;
+            }
+            if (y > 550 && localDot.y > 3) {
+                localDot.y -= 1 * localDot.multiplier;
+            }
+
+            uint32_t buttons = ss.digitalReadBulk(button_mask);
+            unsigned long currentTime = millis();
+
+            if (!(buttons & (1UL << BUTTON_SELECT))) {
+                if (currentTime - lastSelectPress >= 500) {
+                    Point rand = getRandomNum();
+                    localDot.x = rand.x;
+                    localDot.y = rand.y;
+                    lastSelectPress = currentTime;
+                }
+            }
+
+            if (!(buttons & (1UL << BUTTON_START))) {
+                if (localDot.multiplier < 5) {
+                    localDot.multiplier++;
+                } else {
+                    localDot.multiplier = 1;
+                }
+            }
+
+            if (sqrt(pow(clientDot.x - localDot.x, 2) + pow(clientDot.y - localDot.y, 2)) <= 20) {
                 isGameOver = true;
                 endTime = millis();
             }
-    
-        M5.Lcd.fillRect(blueDotX, blueDotY, 10, 10, BLUE);  // Blue dot (joystick control)
-        M5.Lcd.fillRect(pinkDotX, pinkDotY, 10, 10, PINK);    // Red dot (button control)
-        }else
-        {
-           M5.Lcd.setCursor(M5.Lcd.width() / 3, M5.Lcd.height() / 2);
+
+        } else {
+            M5.Lcd.setCursor(M5.Lcd.width() / 3, M5.Lcd.height() / 2);
             M5.Lcd.setTextSize(3);
             M5.Lcd.printf("GAME OVER");
-    
+
             int timeDiff = endTime - startTime;
             M5.Lcd.setCursor(M5.Lcd.width() / 3, M5.Lcd.height() / 2 + 50);
-            M5.Lcd.printf( "%ss",millisToTime(timeDiff));
+            M5.Lcd.printf("%ss", millisToTime(timeDiff));
+            
         }
-    }else if(previouslyConnected){
-        drawScreenTextWithBackground("Disconnected. Reset M5 device to reinitialize BLE.", TFT_RED); // Give feedback on screen
-        timer = 0;
     }
 }
 
+///////////////////////////////////////////////////////////////
 // Colors the background and then writes the text on top
+///////////////////////////////////////////////////////////////
 void drawScreenTextWithBackground(String text, int backgroundColor) {
     M5.Lcd.fillScreen(backgroundColor);
     M5.Lcd.setCursor(0,0);
     M5.Lcd.println(text);
 }
 
-// This code creates the BLE server and broadcasts it
-void broadcastBleServer() {    
-    // Initializing the server, a service and a characteristic 
-    bleServer = BLEDevice::createServer();
-    bleServer->setCallbacks(new MyServerCallbacks());
-    bleService = bleServer->createService(SERVICE_UUID);
-    
-    // Add the write callback to the characteristic
-    bleCharacteristic = bleService->createCharacteristic(CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_WRITE |
-        BLECharacteristic::PROPERTY_NOTIFY |
-        BLECharacteristic::PROPERTY_INDICATE
-    );
-    bleCharacteristic->setValue("Hello BLE World from Dr. Dan!");
-    bleCharacteristic->setCallbacks(new MyCharacteristicCallbacks());  // Set the write callback here
-    bleService->start();
 
-    // Start broadcasting (advertising) BLE service
-    BLEAdvertising *bleAdvertising = BLEDevice::getAdvertising();
-    bleAdvertising->addServiceUUID(SERVICE_UUID);
-    bleAdvertising->setScanResponse(true);
-    bleAdvertising->setMinPreferred(0x12); // Use this value most of the time 
-    BLEDevice::startAdvertising();
-    Serial.println("Characteristic defined...you can connect with your phone!"); 
+///////////////////////////////////////////////////////////////
+// Gets random number for X and Y coords
+///////////////////////////////////////////////////////////////
+Point getRandomNum(){
+    int randomNumberX = random(0, M5.Lcd.width() - 3);
+    int randomNumberY = random(0, M5.Lcd.height() - 3);
+
+    return Point(randomNumberX,randomNumberY);
+
+    // Serial.printf("Random number: %d\n", randomNumberX);
+    // Serial.printf("Random number: %d\n", randomNumberY);
+
 }
 
+///////////////////////////////////////////////////////////////
+// Converts miliseconds to seconds and miliseconds ss:mm
+///////////////////////////////////////////////////////////////
 String millisToTime(unsigned long millisVal) {
     unsigned long seconds = millisVal / 1000;
     unsigned long milliseconds = millisVal % 1000;
@@ -238,14 +272,4 @@ String millisToTime(unsigned long millisVal) {
     sprintf(timeString, "%02lu:%03lu", seconds, milliseconds);
 
     return String(timeString);
-}
-
-Point getRandomNum(){
-    int randomNumberX = random(0, M5.Lcd.width() - 3);
-    int randomNumberY = random(0, M5.Lcd.height() - 3);
-
-    return Point(randomNumberX,randomNumberY);
-
-    Serial.printf("Random number: %d\n", randomNumberX);
-    Serial.printf("Random number: %d\n", randomNumberY);
 }
